224415302bdf839b6bf500b4f0bc29993dc41f61
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-17 09:56:29 +0200

    add change of two files

--- part2/hbnb/app/models/place.py
+++ part2/hbnb/app/models/place.py
@@ -4,34 +4,49 @@ from __init__ import BaseModel
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
-        self.title = title
-        self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
+        self._title = title
+        self._description = description
+        self._price = price
+        self._latitude = latitude
+        self._longitude = longitude
         self.owner = owner
         self.reviews = []  # List to store related reviews
-        self.amenities = [] # List to store related amenities
+        self.amenities = []  # List to store related amenities
         self.users = []
 
-    def title(self, title):
-        if len(title) > 100:
+    @property
+    def title(self):
+        return self._title
+
+    @title.setter
+    def title(self, value):
+        if len(value) > 100:
             raise TypeError("Error: title is invalid")
-        self.title = title
-        
-    def descritption(self, description):
-        self.description = description
-        
-    def price(self, price):
-        if price < 0:
-            raise ValueError("Price can't negatif")
-        self.price = price
-        
-    def coordinate(self, latitude, longitude):
-        if not -90 <= latitude <= 90 or -180 <= longitude <= 180:
-            raise ValueError(" coordinate of latitude and longitude aren't correct")
-        self.latitude = latitude
-        self.longitude = longitude
+        self._title = value
+
+    @property
+    def description(self):
+        return self._description
+
+    @description.setter
+    def description(self, value):
+        self._description = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if value < 0:
+            raise ValueError("Price can't be negative")
+        self._price = value
+
+    def set_coordinates(self, latitude, longitude):
+        if not -90 <= latitude <= 90 or not -180 <= longitude <= 180:
+            raise ValueError("Coordinates of latitude and longitude aren't correct")
+        self._latitude = latitude
+        self._longitude = longitude
 
     def add_review(self, review):
         """Add a review to the place."""
@@ -40,7 +55,7 @@ class Place(BaseModel):
     def add_amenity(self, amenity):
         """Add an amenity to the place."""
         self.amenities.append(amenity)
-        
+
     def add_user(self, user):
-        """Add an user to the place"""
+        """Add a user to the place"""
         self.users.append(user)
