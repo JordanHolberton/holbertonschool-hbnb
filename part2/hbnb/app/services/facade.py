0677b2158ff6be109c9eb226939ec07913fd27b4
JordanHolberton <jordan.chatillon76@laposte.net>
2024-10-21 16:08:26 +0200

    Delete useless code

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -35,18 +35,4 @@ class HBnBFacade:
             self.user_repo.update(user)
         return user
 
-    # Placeholder method for fetching a place by ID
-    def get_place(self, place_id):
-        # Logic will be implemented in later tasks
-        pass
 
-    def create_place(self, place_data):
-        # Placeholder for logic to create a place, including validation for price, latitude, and longitude
-        pass
-
-    def get_all_places(self):
-        # Placeholder for logic to retrieve all places
-        pass
-    def update_place(self, place_id, place_data):
-        # Placeholder for logic to update a place
-        pass
