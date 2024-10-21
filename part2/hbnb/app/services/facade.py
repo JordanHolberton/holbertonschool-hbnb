37081e51d972c1a62747d3c8f7051504a842a6be
Aymeric154 <aymeric.gelle@outlook.fr>
2024-10-21 16:12:55 +0200

    delete elements in facade.py

--- part2/hbnb/app/services/facade.py
+++ part2/hbnb/app/services/facade.py
@@ -1,6 +1,5 @@
 from app.persistence.repository import InMemoryRepository
 from app.models.user import User
-from app.models.amenity import Amenity
 from app.models.place import Place
 
 
@@ -10,40 +9,6 @@ class HBnBFacade:
         self.place_repo = InMemoryRepository()
         self.review_repo = InMemoryRepository()
         self.amenity_repo = InMemoryRepository()
-
-    def create_user(self, user_data):
-        user = User(**user_data)
-        self.user_repo.add(user)
-        return user
-
-    def get_user(self, user_id):
-        return self.user_repo.get(user_id)
-
-    def get_user_by_email(self, email):
-        return self.user_repo.get_by_attribute('email', email)
-
-    def get_all_users(self):
-        return self.user_repo.get_all()
-
-    def create_amenity(self, amenity_data):
-        # Placeholder for logic to create an amenity
-        amenity = Amenity(**amenity_data)
-        self.amenity_repo.add(amenity)
-        return amenity
-
-    def get_amenity(self, amenity_id):
-        # Placeholder for logic to retrieve an amenity by ID
-        return self.amenity_repo.get(amenity_id)
-
-    def get_all_amenities(self):
-        # Placeholder for logic to retrieve all amenities
-        return self.amenity_repo.get_all()
-
-    def update_amenity(self, amenity_id, amenity_data):
-        # Placeholder for logic to update an amenity
-        amenity = self.get_amenity(amenity_id)
-        amenity.update(amenity_data)
-        self.amenity_repo.update(amenity)
 
 
     def create_place(self, place_data):
@@ -74,27 +39,3 @@ class HBnBFacade:
             place.owner_id = place_data['owner_id']
             self.place_repo.update(place)
         return place
-    
-    def create_review(self, review_data):
-    # Placeholder for logic to create a review, including validation for user_id, place_id, and rating
-        pass
-
-    def get_review(self, review_id):
-        # Placeholder for logic to retrieve a review by ID
-        pass
-
-    def get_all_reviews(self):
-        # Placeholder for logic to retrieve all reviews
-        pass
-
-    def get_reviews_by_place(self, place_id):
-        # Placeholder for logic to retrieve all reviews for a specific place
-        pass
-
-    def update_review(self, review_id, review_data):
-        # Placeholder for logic to update a review
-        pass
-
-    def delete_review(self, review_id):
-        # Placeholder for logic to delete a review
-        pass
