748f16500801f36b6df12cdecbc0bd2f4a561c5c
JordanHolberton <jordan.chatillon76@laposte.net>
2024-11-18 09:05:43 +0100

    Add admin check for user registration endpoint

--- part3/hbnb/app/__init__.py
+++ part3/hbnb/app/__init__.py
@@ -3,6 +3,8 @@ from flask_restx import Api
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
 from flask_sqlalchemy import SQLAlchemy
+import uuid
+from datetime import datetime
 
 # Create here to avoid circular imports
 bcrypt = Bcrypt()
